6cbef0dd598715a1bf0e034409df1a89450b05c1
arlost66 <ashimoyu@gmail.com>
2023-02-28 00:50:25 +0800

    Added ambient_temperature to blynk.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <Arduino.h>
 #include <TimeLib.h>
+#include <DHT.h>
+
 #define BLYNK_TEMPLATE_ID "TMPLrF3QZtDg"
 #define BLYNK_DEVICE_NAME "NODEMCU"
 #define BLYNK_FIRMWARE_VERSION        "0.1.0"
 #define BLYNK_PRINT Serial
 #define APP_DEBUG
-#define soil_humidity_pin A0 //v1
+
+#define soil_humidity_pin A0 //v2
+#define ambient_temperature_pin D2 //v3
+#define DHTTYPE DHT22 
+
+DHT dht(ambient_temperature_pin, DHTTYPE);
 #include "BlynkEdgent.h"
 
 BlynkTimer timer;
@@ -21,12 +28,16 @@ String startingTimeString;
 
 //Sensor
 int soil_humidity_value;
+int ambient_temperature_value;
+
+//timer ID holder
+int soil_humidity_ID;
+int ambient_temperature_ID;
 
 //mapping
 int soil_humidity_map;
 
-//timer ID holder
-int soil_humidity_ID;
+
 
 
 
@@ -39,6 +50,13 @@ void soil_humidity() //check mapping
   soil_humidity_map = map(soil_humidity_value, 0, 1023, 0, 100);
   Blynk.virtualWrite(V2, soil_humidity_map);
   Serial.println("Soil Humidity: " + String(soil_humidity_map));
+}
+
+void ambient_temperature()
+{
+  ambient_temperature_value = dht.readTemperature();
+  Blynk.virtualWrite(V3,ambient_temperature_value );
+  Serial.println("DHT: " + String(ambient_temperature_value));
 }
 
 
@@ -59,7 +77,9 @@ BLYNK_WRITE(V0) //turn on switch
       Serial.println("All timer on " + String(year()));
       Blynk.virtualWrite(V5,startingTimeString); // sends string in V5
       Blynk.virtualWrite(V6, startingTime); //UNIX value in longlong
+
       timer.enable(soil_humidity_ID); //enabling the timer for soil humidity
+      timer.enable(ambient_temperature_ID); //enabling the timer for ambient temperature
     }
     digitalWrite(2, HIGH);
     Serial.println("date "+ String(startingTime) + "\n phtime: " + String(now()) );
@@ -82,7 +102,9 @@ BLYNK_WRITE(V7)
       startingTimeString ="Not Started";
       Blynk.virtualWrite(V5,startingTimeString); // sends string in V5
       Blynk.virtualWrite(V6, startingTime); //UNIX value in longlong
+
       timer.disable(soil_humidity_ID); //disabling timer for soil humidty
+      timer.disable(ambient_temperature_ID); //disabling ambient_temperature ID
     }
   } 
 }
@@ -113,7 +135,9 @@ void setup() {
 
   timer.setTimeout(3600000L, [] () {} ); // dummy/sacrificial Function
   soil_humidity_ID = timer.setInterval(2000L, soil_humidity);
+  ambient_temperature_ID = timer.setInterval(2500L, ambient_temperature);
   Serial.begin(115200);
+  dht.begin();
   BlynkEdgent.begin();
 }
 
